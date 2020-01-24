3fd27ecca71f6c520dfffb1bc93f02b19346d8c9
鶴見昌之 <tsuruwatcher@gmail.com>
2020-01-24 16:53:29 +0900

    ゲームパッドの入力追加

--- MyDirectXLib/Project1/Source/Device/Input.cpp
+++ MyDirectXLib/Project1/Source/Device/Input.cpp
@@ -2,27 +2,33 @@
 #include <sstream>
 #include "Def\Screen.h"
 #include "Device\Camera.h"
+#include "Math\MathUtility.h"
 
-#define KEY_MAX 256
+BYTE Input::m_CurKeys[256];
+BYTE Input::m_PreKeys[256];
 
-static BYTE mCurKeys[KEY_MAX];
-static BYTE mPreKeys[KEY_MAX];
+JOYINFOEX Input::m_CurPadInfo;
+JOYINFOEX Input::m_PrePadInfo;
+
+bool Input::m_isPadConnected = false;
 
 #define PRESSED 0x80
+#define JOYSTICK_CENTER 32767.0f
+#define JOYSTICK_MAX 65535.0f
 
 bool Input::isKeyUp(UINT key)
 {
-	return !(mCurKeys[key] & PRESSED) && (mPreKeys[key] & PRESSED);
+	return !(m_CurKeys[key] & PRESSED) && (m_PreKeys[key] & PRESSED);
 }
 
 bool Input::isKey(UINT key)
 {
-	return (mCurKeys[key] & PRESSED) && (mPreKeys[key] & PRESSED);
+	return (m_CurKeys[key] & PRESSED) && (m_PreKeys[key] & PRESSED);
 }
 
 bool Input::isKeyDown(UINT key)
 {
-	return (mCurKeys[key] & PRESSED) && !(mPreKeys[key] & PRESSED);
+	return (m_CurKeys[key] & PRESSED) && !(m_PreKeys[key] & PRESSED);
 }
 
 Vec3 Input::getMousePosition()
@@ -44,8 +50,127 @@ Vec3 Input::getWorldMousePosition()
 	return getMousePosition() - Camera::getPosition();
 }
 
+bool Input::isPadButtonUp(PAD_BUTTON button)
+{
+	return m_isPadConnected && !(m_CurPadInfo.dwButtons & button) && (m_PrePadInfo.dwButtons & button);
+}
+
+bool Input::isPadButton(PAD_BUTTON button)
+{
+	return m_isPadConnected && (m_CurPadInfo.dwButtons & button) && (m_PrePadInfo.dwButtons & button);
+}
+
+bool Input::isPadButtonDown(PAD_BUTTON button)
+{
+	return  m_isPadConnected && (m_CurPadInfo.dwButtons & button) && !(m_PrePadInfo.dwButtons & button);
+}
+
+Vec2 Input::getDPadValue()
+{
+	if (!m_isPadConnected)
+		return Vec2::zero();
+
+	Vec2 axis;
+	//左入力されていたら
+	if (m_CurPadInfo.dwPOV == JOY_POVLEFT)
+	{
+		axis.x = -1;
+		axis.y = 0;
+	}
+
+	//右入力されていたら
+	if (m_CurPadInfo.dwPOV == JOY_POVRIGHT)
+	{
+		axis.x = 1;
+		axis.y = 0;
+	}
+
+	//上入力されていたら
+	if (m_CurPadInfo.dwPOV == JOY_POVFORWARD)
+	{
+		axis.x = 0;
+		axis.y = 1;
+	}
+
+	//上入力されていたら
+	if (m_CurPadInfo.dwPOV == JOY_POVBACKWARD)
+	{
+		axis.x = 0;
+		axis.y = -1;
+	}
+
+	return axis;
+}
+
+Vec2 Input::getLStickValue()
+{
+	if (!m_isPadConnected)
+		return Vec2::zero();
+
+	Vec2 axis(0, 0);
+	//-JOYSTICK_CENTER ~ +JOYSTICK_CENTERの間に値を修正
+	float centeredX = (m_CurPadInfo.dwXpos - JOYSTICK_CENTER);
+	float centeredY = (m_CurPadInfo.dwYpos - JOYSTICK_CENTER);
+
+	//-1 ~ 1の間に変換
+	if (centeredX != 0)
+		axis.x = MathUtility::clamp(centeredX / JOYSTICK_CENTER, -1.0f, 1.0f);
+
+	//-1 ~ 1の間に変換(Yの場合は符号を逆にする)
+	if (centeredY != 0)
+		axis.y = MathUtility::clamp(centeredY / JOYSTICK_CENTER, -1.0f, 1.0f) * -1.0f;
+
+	return axis;
+}
+
+Vec2 Input::getRStickValue()
+{
+	if (!m_isPadConnected)
+		return Vec2::zero();
+
+	Vec2 axis(0, 0);
+	//-JOYSTICK_CENTER ~ +JOYSTICK_CENTERの間に値を修正
+	float centeredZ = (m_CurPadInfo.dwZpos - JOYSTICK_CENTER);
+	float centeredR = (m_CurPadInfo.dwRpos - JOYSTICK_CENTER);
+
+	//-1 ~ 1の間に変換
+	if (centeredZ != 0)
+		axis.x = MathUtility::clamp(centeredZ / JOYSTICK_CENTER, -1.0f, 1.0f);
+
+	//-1 ~ 1の間に変換(Yの場合は符号を逆にする)
+	if (centeredR != 0)
+		axis.y = MathUtility::clamp(centeredR / JOYSTICK_CENTER, -1.0f, 1.0f) * -1.0f;
+
+	return axis;
+}
+
 void Input::update()
 {
-	memcpy_s(mPreKeys, sizeof(mPreKeys), mCurKeys, sizeof(mCurKeys));
-	GetKeyboardState(mCurKeys);
+	//前フレームのキーボード入力をコピー
+	memcpy_s(m_PreKeys, sizeof(m_PreKeys), m_CurKeys, sizeof(m_CurKeys));
+	//現在のフレームのキーボード入力を取得
+	GetKeyboardState(m_CurKeys);
+
+	//前フレームの状態をコピー
+	m_PrePadInfo = m_CurPadInfo;
+
+	//取得したいデータのサイズ指定(RETURNALLを指定しているので構造体と同じ)
+	m_CurPadInfo.dwSize = sizeof(JOYINFOEX);
+	m_CurPadInfo.dwFlags = JOY_RETURNALL;
+
+	bool noError = false;
+
+	m_isPadConnected = false;
+
+	//接続されているコントローラーを探す
+	for (int i = 0; i < joyGetNumDevs(); i++)
+	{
+		//問題なく接続されているコントローラーが見つかったら終了
+		if (joyGetPosEx(i, &m_CurPadInfo) == JOYERR_NOERROR)
+		{
+			noError = true;
+			m_isPadConnected = true;
+			break;
+		}
+	}
 }
--- MyDirectXLib/Project1/Source/Device/Input.h
+++ MyDirectXLib/Project1/Source/Device/Input.h
@@ -1,17 +1,64 @@
 #pragma once
 #include <Windows.h>
-#include "Math/Vec3.h"
+#include <joystickapi.h>
+#include "Math\Vec3.h"
+#include "Math\Vec2.h"
+
+//ジョイスティック使用に必要なライブラリをリンク
+#pragma comment(lib, "Winmm.Lib")
 
 class Input
 {
 public:
+	//コントローラーのボタン指定用Enum
+	enum PAD_BUTTON
+	{
+		PAD_BUTTON_X = 0x0001,			//Xボタン
+		PAD_BUTTON_Y = 0x0002,			//Yボタン
+		PAD_BUTTON_A = 0x0004,			//Aボタン
+		PAD_BUTTON_B = 0x0008,			//Bボタン
+		PAD_BUTTON_LB = 0x00000010l,	//Lボタン
+		PAD_BUTTON_RB = 0x00000020l,	//Rボタン
+		PAD_BUTTON_LT = 0x00000040l,	//Lトリガー
+		PAD_BUTTON_RT = 0x00000080l,	//Rトリガー
+		PAD_BUTTON_START = 0x00000800l,	//スタートボタン
+	};
+
+	//指定されたキーが離された時にTrueを返します
 	static bool isKeyUp(UINT key);
+	//指定されたキーが押され続けている時にTrueを返します
 	static bool isKey(UINT key);
+	//指定されたキーが押された時にTrueを返します
 	static bool isKeyDown(UINT key);
 
+	//マウスの座標を返します
 	static Vec3 getMousePosition();
+	//カメラ基準でマウス座標を返します
 	static Vec3 getWorldMousePosition();
 
+	//コントローラーのボタンが離された時にTrueを返します
+	static bool isPadButtonUp(PAD_BUTTON button);
+	//コントローラーのボタン押され続けている時にTrueを返します
+	static bool isPadButton(PAD_BUTTON button);
+	//コントローラーのボタンが押された時にTrueを返します
+	static bool isPadButtonDown(PAD_BUTTON button);
+
+	//十字ボタン(名前がわからない)の入力を(-1 ~ 1, -1 ~ 1)の範囲で取得します
+	static Vec2 getDPadValue();
+	//左スティックの入力を(-1 ~ 1, -1 ~ 1)の範囲で取得します
+	static Vec2 getLStickValue();
+	//右スティックの入力を(-1 ~ 1, -1 ~ 1)の範囲で取得します
+	static Vec2 getRStickValue();
+
 	static void update();
+
+private:
+	static BYTE m_CurKeys[256];
+	static BYTE m_PreKeys[256];
+
+	static JOYINFOEX m_CurPadInfo;
+	static JOYINFOEX m_PrePadInfo;
+
+	static bool m_isPadConnected;
 };
 
